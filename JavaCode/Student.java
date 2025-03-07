a669209923ce28a7d4a68eefadfa23d522a82ff9
K-Ammad <khawajaammad711@gmail.com>
2025-03-07 11:58:02 +0000

    Studetn 3rd commit

--- JavaCode/Student.java
+++ JavaCode/Student.java
@@ -4,21 +4,48 @@ public class Student
 	private int studentId;
 	private double mark;
 
+//Constructor
 
-public boolean isValidStudentName(String pStudentName)  // isValidStudentName method
+public Student (String pStudentName , int pStudentId , double pMark)
 {
-    	return pStudentName != null &&  !pStudentName.isEmpty();
-}
+	boolean isValid = true;
 
+	if(isValidStudentName(pStudentName))
+	{	
+		studentName = pStudentName;
+	}
+	else
+	{
+		System.out.println("Invalid Name. Name cannot be empty");
+		isValid = false;
+	}
 
-public boolean isValidStudentId(int pStudentId) //isValidStudentId method
-{
-	return pStudentId >=10000000 && pStudentId <=99999999;
-}
+	if(isValidStudentId(pStudentId))
+	{
+		studentId = pStudentId;
+	}
+	else
+	{
+		System.out.println("Invalid Student ID");
+		isValid = false;
+	}
 
-public boolean isValidMark(double pMark)   // isValidMark method
-{
-	return pMark >=0.0 && pMark <= 100.0;
+	if(isValidMark(pMark))
+	{
+		mark = pMark;
+	}
+	else
+	{
+		System.out.println("Invalid Marks");
+		isValid = false;
+	}
+
+	if(!isValid)
+	{
+		studentName = null;
+		studentId = 0;
+		mark = 0.0;
+	}
 }
 
 //Getters
@@ -73,49 +100,59 @@ public void setMark(double pMark)
 	}	
 }
 
-//Constructor
+public String getGrade(double pMark)	//getGrade Method
+{ 
+	if (pMark >= 0 && pMark <= 49.9)
+		{
+		       	return "F";
+		}
 
-public Student (String pStudentName , int pStudentId , double pMark)
-{
-	boolean isValid = true;
+	else if (pMark >= 50.0 && pMark <= 59.9)
+		{
+		       	return "P";
+		}
 
-	if(isValidStudentName(pStudentName))
-	{	
-		studentName = pStudentName;
-	}
+ 	else if (pMark >= 60.0 && pMark <= 69.9)
+		{
+		       	return "C";
+		}
+
+	else if (pMark >= 70.0 && pMark <= 79.9)
+		{
+		       	return "D";
+		}
+
+	else if (pMark >= 80.0 && pMark <= 100.0)
+		{
+		       	return "HD";
+		}
 	else
-	{
-		System.out.println("Invalid Name. Name cannot be empty");
-		isValid = false;
-	}
-
-	if(isValidStudentId(pStudentId))
-	{
-		studentId = pStudentId;
-	}
-	else
-	{
-		System.out.println("Invalid Student ID");
-		isValid = false;
-	}
-
-	if(isValidMark(pMark))
-	{
-		mark = pMark;
-	}
-	else
-	{
-		System.out.println("Invalid Marks");
-		isValid = false;
-	}
-
-	if(!isValid)
-	{
-		studentName = null;
-		studentId = 0;
-		mark = 0.0;
-	}
+		{
+			return "Invalid Marks";
+		}
 }
+
+
+public boolean isValidStudentName(String pStudentName)  // isValidStudentName method
+{
+    	return pStudentName != null && !pStudentName.isEmpty();
+}
+
+public boolean isValidStudentId(int pStudentId) //isValidStudentId method
+{
+	return pStudentId >=10000000 && pStudentId <=99999999;
+}
+
+public boolean isValidMark(double pMark)   // isValidMark method
+{
+	return pMark >=0.0 && pMark <= 100.0;
+}
+
+public String toStringFile()
+{
+	return studentName + "," + studentId + "," + mark;
+}
+
 
 public static void main (String[] args)
 	{
@@ -123,25 +160,24 @@ public static void main (String[] args)
 	Student s1 = new Student("Alice" , 12450000, 78.5);
 	System.out.println("Student Name: "+ s1.getStudentName());
 	System.out.println("Student ID: "  + s1.getStudentId());
-	System.out.println("marks: "       + s1.getMark());
+	System.out.println("marks: " + s1.getMark()+ " " +"Grade: "+s1.getGrade(s1.getMark()));
+	System.out.println(s1.toStringFile() + "\n");
 
-
-
-	Student s2 = new Student("", 251, -10);
+	
+	Student s2 = new Student("Robby",19402489 ,90.5 );
 	System.out.println("Student Name: "+ s2.getStudentName());
 	System.out.println("Student ID: "  + s2.getStudentId());
-	System.out.println("marks: "       + s2.getMark());
+	System.out.println("marks: "+s2.getMark()+ " " + "Grade: "+ s2.getGrade(s2.getMark()));
+	System.out.println(s2.toStringFile() + "\n");
 	
-
-
-	s2.setStudentName("Bob");
-	s2.setStudentId(99999993);
-	s2.setMark(23);
-
+	s2.setStudentName("Chase");
+	s2.setStudentId(17389256);
+	s2.setMark(55);
+	
 	System.out.println("Student Name: "+ s2.getStudentName());
 	System.out.println("Student ID: "  + s2.getStudentId());
-	System.out.println("marks: "       + s2.getMark());
-	
+	System.out.println("marks: "+s2.getMark()+ " " + "Grade: "+ s2.getGrade(s2.getMark()));
+	System.out.println(s2.toStringFile() + "\n");
 	}
 
 }	
